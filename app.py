857cdd5330951ff04cf0faf152c3c2d8fbc76f48
SayamAlt <sayamk565@gmail.com>
2023-10-26 18:46:16 +0530

    first commit

--- app.py
+++ app.py
@@ -5,6 +5,9 @@ import pandas as pd
 app = Flask(__name__)
 
 pipeline = joblib.load('pipeline.pkl')
+transformer = pipeline.named_steps['transformer']
+scaler = pipeline.named_steps['scaler']
+model = pipeline.named_steps['model']
 
 @app.route("/")
 def home():
@@ -19,7 +22,9 @@ def predict():
         tip = int(request.form['tip'])
         base_fare = float(request.form['base_fare'])
         data = pd.DataFrame([[meter_rate,trip_duration,miscellaneous_fees,tip,base_fare]],columns=['meter_rate','trip_duration','miscellaneous_fees','tip','base_fare'])
-        pred = pipeline.predict(data)[0]
+        transformed_data = transformer.transform(data)
+        scaled_data = scaler.transform(transformed_data)
+        pred = model.predict(scaled_data)
         output = '%.2f' % round(pred, 2)
         return render_template('index.html',prediction_text=f"The predicted total fare of your taxi trip is ₹{output}.")
     
